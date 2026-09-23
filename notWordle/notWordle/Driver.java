732bbe68203a2a9529b09787f7d7aa2427b8c078
Colby Moore <cmoore15@uafs.edu>
2026-09-23 10:04:25 -0500

    Start

---
+++ notWordle/notWordle/Driver.java
@@ -0,0 +1,11 @@
+package notWordle;
+
+import java.io.*;
+
+public class Driver {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new FileReader("notWordleHighscore.txt"));
+	}
+}
---
+++ notWordle/notWordle/NotWordle.java
@@ -0,0 +1,5 @@
+package notWordle;
+
+public class NotWordle {
+
+}
