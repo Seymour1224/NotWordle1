6f5dc854eb85d7375e57b6d00a59a6fa605b130a
Colby Moore <cmoore15@uafs.edu>
2026-09-23 10:56:53 -0500

    started0.1

--- notWordle/notWordle/Driver.java
+++ notWordle/notWordle/Driver.java
@@ -1,11 +1,21 @@
 package notWordle;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Driver {
 
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new FileReader("notWordleHighscore.txt"));
+		Scanner scan = new Scanner(System.in);
+		String nickName;
+		
+		System.out.print("What is Your Name");
+		//nickName = scan.nextLine();
+		
+		NotWordle word = new NotWordle();
+		word.show(word.randomize());
+		
 	}
 }
--- notWordle/notWordle/NotWordle.java
+++ notWordle/notWordle/NotWordle.java
@@ -1,5 +1,40 @@
 package notWordle;
 
+import java.util.Random;
+
 public class NotWordle {
 
+	
+	public NotWordle() {
+		
+	}
+	
+	public int[] randomize() {
+		int[] sort = {1,2,3,4,5};
+		int temp;
+		int index;
+		Random rand = new Random();
+		int index1;
+		int index2;
+		
+		
+		for(index = 0; index < rand.nextInt(10)+20; index++) {
+			index1 = rand.nextInt(5);
+			index2 = rand.nextInt(5);
+			
+			temp = sort[index1];
+			sort[index1] = sort[index2];
+			sort[index2] = temp;
+			
+		}
+		return sort;
+	}
+	
+	public void show(int[] theArray) {
+		int index;
+		
+		for(index = 0; index < theArray.length; index++) {
+			System.out.printf("[%d]  ", theArray[index]);
+		}
+	}
 }
